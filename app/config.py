578759a9af0146f134fe5db0b28e22e9c4f575c9
Ghani Zulhusni Bahri <ghanizulhusnib@gmail.com>
2026-09-18 13:12:45 +0700

    Add shared config and schemas

---
+++ app/config.py
@@ -0,0 +1,29 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+
+    APP_ENV: str = "development"
+    APP_HOST: str = "0.0.0.0"
+    APP_PORT: int = 8000
+
+    SUPABASE_URL: str = ""
+    SUPABASE_ANON_KEY: str = ""
+    SUPABASE_SERVICE_ROLE_KEY: str = ""
+
+    LIVEKIT_URL: str = ""
+    LIVEKIT_API_KEY: str = ""
+    LIVEKIT_API_SECRET: str = ""
+    LIVEKIT_STT_MODEL: str = "deepgram/nova-3"
+    LIVEKIT_TTS_MODEL: str = "cartesia/sonic-3"
+    LIVEKIT_TTS_VOICE: str = "9626c31c-bec5-4cca-baa8-f8ba9e84c8bc"
+
+    NINEROUTER_BASE_URL: str = "http://127.0.0.1:20128/v1"
+    NINEROUTER_API_KEY: str = ""
+    NINEROUTER_MODEL: str = "ghn-fast"
+
+    MAX_CONTEXT_MESSAGES: int = 20
+
+
+settings = Settings()
